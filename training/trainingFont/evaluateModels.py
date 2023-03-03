efae69d63ba56ba116710b7fd621843faeb9b74c
JosedaMachine <jrave@ucm.es>
2023-03-03 23:22:58 +0100

    Estructura para la evaluacion de 3 modelos

--- training/trainingFont/evaluateModels.py
+++ training/trainingFont/evaluateModels.py
@@ -103,6 +103,48 @@ def clearModel(lenguage, font_Name):
             f'{font_Name}',
             ])
 
+def modelEvaluation(kf,archivos_ordenados, groundTruthPath, temp_folder, lenguage, font_Name, resultFile):
+    percentage = 0.
+    steps = 1.0/5.0
+    for train_index, test_index in kf.split(archivos_ordenados):
+        # if firstTime == False:
+        #     firstTime = True
+        # else: 
+        #     return
+        #Mover
+        for file in test_index:
+            subprocess.run(['mv', '-n',f'{groundTruthPath}/{archivos_ordenados[file]}',  f'{temp_folder}'])
+
+        # archivos = sorted(os.listdir(temp_folder))
+        # print(archivos)
+
+        # Entrenar 
+        subprocess.run([
+            'python',
+            'trainTess.py',
+            '-l',
+            f'{lenguage}',
+            '-f',
+            f'{font_Name}',
+            '-it',
+            '100'
+            ])
+
+        # #Devolver a carpeta
+        for file in test_index:
+            file = archivos_ordenados[file]
+            #Devolver a carpeta
+            subprocess.run(['mv', '-n',f'{temp_folder}/{file}', f'{groundTruthPath}'])
+
+        #Extraer datos y esribir en archivo de resultados
+        extract_compare_Data(archivos_ordenados, test_index, groundTruthPath, font_Name, lenguage, resultFile)
+
+        #Limpiar modelo 
+        clearModel(lenguage, font_Name)
+
+        percentage = percentage + steps
+        print(f"\033[33mEvaluation:{round((percentage*100),2)}% of 100%\033[0m")
+
 def evaluate(lenguage, font_Name):
     groundTruthPath = f'{tesstrain_Folder}/data/{font_Name}_data/{font_Name}-ground-truth/{lenguage}'
 
@@ -127,68 +169,44 @@ def evaluate(lenguage, font_Name):
     archivos_ordenados = sorted(archivos)
     # print(archivos_ordenados)
 
-    resultFile = {}
-    # resultFile = open(f"{result_folder}/results.txt", "w")
-
+    #Generamos particion de 5 grupos para la evaluación.
     kf = KFold(n_splits=5)
     #Entrenamiento y evaluacion
     
-    percentage = 0.
-    steps = 1.0/5.0
+    #Modelos
+    
+    resultFile = {}
+    # A: Default Tesseract
+    #Este solo va a ejecutar no? sin entrenar. solo probar.
+    modelEvaluation(kf, archivos_ordenados, groundTruthPath, temp_folder, lenguage, font_Name, resultFile)
 
-    #Nos aseguramos de que no haya nada entrenado
-    clearModel(lenguage, font_Name)
-
-    # firstTime = False
-
-    print(f"\033[33mEvaluation: {round((percentage*100),2)}% of 100%\033[0m")
-    for train_index, test_index in kf.split(archivos_ordenados):
-        # if firstTime == False:
-        #     firstTime = True
-        # else: 
-        #     return
-        #Mover
-        for file in test_index:
-            subprocess.run(['mv', '-n',f'{groundTruthPath}/{archivos_ordenados[file]}',  f'{temp_folder}'])
-
-        # archivos = sorted(os.listdir(temp_folder))
-        # print(archivos)
-
-        # Entrenar 
-        subprocess.run([
-            'python',
-            'trainTess.py',
-            '-l',
-            f'{lenguage}',
-            '-f',
-            f'{font_Name}',
-            '-it',
-            '400'
-            ])
-
-        # #Devolver a carpeta
-        for file in test_index:
-            file = archivos_ordenados[file]
-            #Devolver a carpeta
-            subprocess.run(['mv', '-n',f'{temp_folder}/{file}', f'{groundTruthPath}'])
-
-        #Extraer datos y esribir en archivo de resultados
-        extract_compare_Data(archivos_ordenados, test_index, groundTruthPath, font_Name, lenguage, resultFile)
-
-        #Limpiar modelo (?)
-        clearModel(lenguage, font_Name)
-
-        percentage = percentage + steps
-        print(f"\033[33mEvaluation:{round((percentage*100),2)}% of 100%\033[0m")
-
-    #Cerramos fichero
-
-    with open(f"{result_folder}/results.txt", "w") as archivo_json:
+    #Escribimos en  fichero y cerramos
+    with open(f"{result_folder}/resultsModel_Default_A.txt", "w") as archivo_json:
         json.dump(resultFile, archivo_json, indent = 4)
-    # resultFile.close()
+    
+    # B: Trained Font overfitted
+    #Limpiamos el ground truth que haya
+    subprocess.run([ 'python', 'groundTruth.py', '-cl', '-l',f'{lenguage}','-f',f'{font_Name}'])
+    #Creamos el ground truth
+    subprocess.run([ 'python', 'groundTruth.py','-l',f'{lenguage}','-f',f'{font_Name}'])
+    resultFile = {}
+    modelEvaluation(kf, archivos_ordenados, groundTruthPath, temp_folder, lenguage, font_Name, resultFile)
+    with open(f"{result_folder}/resultsModel_Font_B.txt", "w") as archivo_json:
+        json.dump(resultFile, archivo_json, indent = 4)
+    
 
-    # # Obtener el primer fold
-    # train_index, test_index = next(kf_iter)
+    # C: Trained Font overfitted to custom grount truth test
+    #Limpiamos el ground truth que haya
+    subprocess.run([ 'python', 'groundTruth.py', '-cl', '-l',f'{lenguage}','-f',f'{font_Name}'])
+    #Creamos ground truth con texto personalizado
+    subprocess.run([ 'python', 'groundTruth.py', '-l',f'{lenguage}','-f',f'{font_Name}', '-dir' , "/home/trainingFont/dialogos/"])
+    
+    resultFile = {}
+    modelEvaluation(kf, archivos_ordenados, groundTruthPath, temp_folder, lenguage, font_Name, resultFile)
+    
+    #Escribimos en fichero y cerramos
+    with open(f"{result_folder}/resultsModel_Custom_C.txt", "w") as archivo_json:
+        json.dump(resultFile, archivo_json, indent = 4)
 
     #Eliminamos el directorio temporal
     shutil.rmtree(temp_folder)
